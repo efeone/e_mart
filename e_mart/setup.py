d1ac057747b7e1b08ddeaf099b2e0b59a81c5bd9
sappu-hub <sarfrasr@gmail.com>
2025-06-30 13:31:44 +0530

    feat:added fields in item doctype and lavanya settings

--- e_mart/setup.py
+++ e_mart/setup.py
@@ -13,6 +13,7 @@ def after_install():
     create_custom_fields(get_sales_invoice_item_custom_fields(), ignore_validate=True, update=True)
     create_custom_fields(get_serial_and_batch_entry_custom_fields(), ignore_validate=True, update=True)
     create_custom_fields(get_purchase_invoice_item_custom_fields(),ignore_validate=True, update=True)
+    create_custom_fields(get_item_custom_fields(),ignore_validate=True, update=True)
 
     create_property_setters(get_property_setters())
 
@@ -27,6 +28,7 @@ def before_uninstall():
     delete_custom_fields(get_sales_invoice_item_custom_fields())
     delete_custom_fields(get_serial_and_batch_entry_custom_fields())
     delete_custom_fields(get_purchase_invoice_item_custom_fields())
+    delete_custom_fields(get_item_custom_fields())
 
 
 def delete_custom_fields(custom_fields: dict):
@@ -118,6 +120,46 @@ def get_purchase_invoice_item_custom_fields():
                 "label": "Schema Discount Amount",
                 "insert_after": "amount"  
             }
+        ]
+    }
+
+def get_item_custom_fields():
+    """
+    Custom fields that need to be added to the Item DocType
+    """
+    return {
+        "Item": [
+            {
+                "fieldname": "type_of_commission",
+                "fieldtype": "Select",
+                "label": "Type Of Commission",
+                "options": "Percentage\nFixed",
+                "insert_after": "stock_uom"
+            },
+            {
+                "fieldname": "commission_value",
+                "fieldtype": "Float",
+                "label": "Commission Value",
+                "insert_after": "type_of_commission"
+			},
+            {
+                "fieldname": "demo_required",
+                "fieldtype": "Check",
+                "label": "Demo Required",
+                "insert_after": "commission_value"
+			},
+            {
+                "fieldname": "periodic_service",
+                "fieldtype": "Check",
+                "label": "Periodic Service",
+                "insert_after": "demo_required"
+			},
+            {
+                "fieldname": "mrp",
+                "fieldtype": "Float",
+                "label": "MRP",
+                "insert_after": "periodic_service"
+			},
         ]
     }
 
