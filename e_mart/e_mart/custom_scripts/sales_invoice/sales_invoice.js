f387933da89d62ec54cf4ea330fd224739d37908
NaseeraVP <naseeravp38@gmail.com>
2025-07-01 12:14:20 +0530

    fix:Removed Unwanted commented code

--- e_mart/e_mart/custom_scripts/sales_invoice/sales_invoice.js
+++ e_mart/e_mart/custom_scripts/sales_invoice/sales_invoice.js
@@ -1,39 +1,3 @@
-// frappe.ui.form.on('Sales Invoice', {
-//     onload: function(frm) {
-//         update_total_buyback_amount(frm);
-//     }
-// });
-
-// frappe.ui.form.on('Buyback Item', {
-//     qty: function(frm, cdt, cdn) {
-//         calculate_row_amount(frm, cdt, cdn);
-//     },
-//     rate: function(frm, cdt, cdn) {
-//         calculate_row_amount(frm, cdt, cdn);
-//     },
-//     buyback_items_remove: function(frm) {
-//         update_total_buyback_amount(frm);
-//     },
-//     buyback_items_add: function(frm) {
-//         update_total_buyback_amount(frm);
-//     }
-// });
-
-// function calculate_row_amount(frm, cdt, cdn) {
-//     let row = locals[cdt][cdn];
-//     row.amount = (row.qty || 0) * (row.rate || 0);
-//     frm.fields_dict.buyback_items.grid.refresh(); // visually refresh
-//     update_total_buyback_amount(frm);
-// }
-
-// function update_total_buyback_amount(frm) {
-//     let total = 0;
-//     (frm.doc.buyback_items || []).forEach(row => {
-//         total += row.amount || 0;
-//     });
-//     frm.set_value('buyback_amount', total);
-// }
-
 frappe.ui.form.on('Sales Invoice', {
     onload(frm) {
         update_total_buyback_amount(frm);
