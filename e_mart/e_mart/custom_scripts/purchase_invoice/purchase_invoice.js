86b192dde7aaf439f68dec03c08e03dbb06e3586
hridyalakshmi <hridyalkrishnan2001@gmail.com>
2025-08-23 10:30:36 +0530

    feat: Clear taxes and recalculates total when purchase category is set to 'Special'

--- e_mart/e_mart/custom_scripts/purchase_invoice/purchase_invoice.js
+++ e_mart/e_mart/custom_scripts/purchase_invoice/purchase_invoice.js
@@ -14,6 +14,41 @@ frappe.ui.form.on('Purchase Invoice', {
 				}
 			}, 150);
 		});
+	},
+	//clears taxes and recalculates totals when Purchase Category is set to "Special"
+	purchase_category: function(frm) {
+		if (frm.doc.purchase_category === 'Special') {
+			frm.set_value('taxes_and_charges', null);
+			
+			frm.clear_table('taxes');
+			frm.refresh_field('taxes');
+			
+			frm.doc.total_taxes_and_charges = 0;
+			frm.doc.taxes_and_charges_added = 0;
+			frm.doc.grand_total = frm.doc.net_total || 0;
+			frm.doc.rounded_total = Math.round(frm.doc.grand_total || 0);
+			frm.doc.outstanding_amount = frm.doc.rounded_total || 0;
+			
+			frm.refresh_field('total_taxes_and_charges');
+			frm.refresh_field('grand_total');
+			frm.refresh_field('rounded_total');
+		}
+	},
+	validate: function(frm) {
+		if (frm.doc.purchase_category === 'Special') {
+			frm.set_value('taxes_and_charges', null);
+			frm.clear_table('taxes');
+			frm.refresh_field('taxes');
+
+			frm.doc.total_taxes_and_charges = 0;
+			frm.doc.taxes_and_charges_added = 0;
+			frm.doc.grand_total = frm.doc.net_total || 0;
+			frm.doc.rounded_total = Math.round(frm.doc.grand_total || 0);
+
+			frm.refresh_field('total_taxes_and_charges');
+			frm.refresh_field('grand_total');
+			frm.refresh_field('rounded_total');
+		}
 	}
 });
 
@@ -51,4 +86,3 @@ function calculate_total_schema_discount(cdt, cdn) {
 		frappe.model.set_value(cdt, cdn, 'total_schema_discount_amount', row.total_schema_discount);
 	}
 }
-		
---
+++ e_mart/e_mart/custom_scripts/purchase_receipt/purchase_receipt.js
@@ -0,0 +1,37 @@
+frappe.ui.form.on('Purchase Receipt', {
+	//clears taxes and recalculates totals when Purchase Category is set to "Special"
+	purchase_category: function(frm) {
+		if (frm.doc.purchase_category === 'Special') {
+			frm.set_value('taxes_and_charges', null);
+			
+			frm.clear_table('taxes');
+			frm.refresh_field('taxes');
+			
+			frm.doc.total_taxes_and_charges = 0;
+			frm.doc.taxes_and_charges_added = 0;
+			frm.doc.grand_total = frm.doc.net_total || 0;
+			frm.doc.rounded_total = Math.round(frm.doc.grand_total || 0);
+			frm.doc.outstanding_amount = frm.doc.rounded_total || 0;
+			
+			frm.refresh_field('total_taxes_and_charges');
+			frm.refresh_field('grand_total');
+			frm.refresh_field('rounded_total');
+		}
+	},
+	validate: function(frm) {
+		if (frm.doc.purchase_category === 'Special') {
+			frm.set_value('taxes_and_charges', null);
+			frm.clear_table('taxes');
+			frm.refresh_field('taxes');
+
+			frm.doc.total_taxes_and_charges = 0;
+			frm.doc.taxes_and_charges_added = 0;
+			frm.doc.grand_total = frm.doc.net_total || 0;
+			frm.doc.rounded_total = Math.round(frm.doc.grand_total || 0);
+
+			frm.refresh_field('total_taxes_and_charges');
+			frm.refresh_field('grand_total');
+			frm.refresh_field('rounded_total');
+		}
+	}
+});
